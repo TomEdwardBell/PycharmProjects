fb2b18fbc7b8ae55be84fe515f0a730614da1d3d
Tom <tomedwardbell@gmaill.com>
2018-10-23 17:26:45 +0100

    Simplified it all

--- Arcade/Arcade.py
+++
@@ -1,8 +0,0 @@
-import ArcadeMineSweeper
-import ArcadeBattleShipsAI
-import ArcadeTicTacToe
-
-while True:
-    ArcadeMineSweeper.start_game()
-    ArcadeTicTacToe.start_game()
-    ArcadeBattleShipsAI.start_game()
